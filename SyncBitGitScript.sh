82d0be4fb312905c202c5e859f64a23ae02cdf20
Kyle McElyea <kmcelyea@Kyles-MacBook-Pro.local>
2016-07-26 11:12:19 -0500

    rework of the script tt

--- SyncBitGitScript.sh
+++ SyncBitGitScript.sh
@@ -1,11 +1,24 @@
 #!/bin/bash
-REPO_NAME=IntegrationTest.git 
+REPO_NAME=IntegrationTest.git
 ORIGIN_URL=git@github.com:Kmcelyea/IntegrationTest.git
-REPO1_URL=git@bitbucket.org:KmcElyea/integrationtest.git 
-rm -rf $REPO_NAME 
-git clone --bare $ORIGIN_URL 
-cd $REPO_NAME 
-git remote add --mirror=fetch repo1 $REPO1_URL 
-git fetch origin --tags 
-git push repo1 --all 
+REPO1_URL=git@bitbucket.org:KmcElyea/integrationtest.git
+if [ -d "$REPO_NAME" ];
+then
+cd $REPO_NAME
+git fetch origin --tags
+git fetch origin master:master
+git push repo1 master
+git fetch origin production:production
+git push repo1 production
 git push repo1 --tags
+else
+git clone --bare $ORIGIN_URL
+cd $REPO_NAME
+git remote add --mirror=fetch repo1 $REPO1_URL
+git fetch origin --tags
+git push repo1 master
+git push repo1 production
+git push repo1 --tags
+fi
+# cd ..
+# rm -rf $REPO_NAME
